9249d723c4fff9c43550d3724ab2f6b1c054a6cc
Adam Mcphee <adkmcphee@gmail.com>
2023-09-28 13:30:50 -0700

    add intial login page

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "../styles/LoginPage.scss";
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    ///Add login to handle login once backend and dB are setup. Google O auth??
+  };
+
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
