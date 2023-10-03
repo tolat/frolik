9d8d8ed1fa9e1ab4c2604b33c1fc204a761de6ab
macuser <macuser@MacBook-Pro.local>
2023-10-03 12:06:01 -0700

    added basic photos and friends section to profile page

--- client/src/components/Pages/Profile.jsx
+++ client/src/components/Pages/Profile.jsx
@@ -5,13 +5,44 @@ import MainContainer from "../UI/MainContainer";
 import { redirect } from "react-router-dom";
 import store from "../../store";
 
+// REMOVE FOR BUILD, DUMMY IMAGES
+import img1 from "../../images/sampleUserImgs/acroyoga.png";
+import img2 from "../../images/sampleUserImgs/beerpong.png";
+import img3 from "../../images/sampleUserImgs/drawing.png";
+import img4 from "../../images/sampleUserImgs/football.png";
+import img5 from "../../images/sampleUserImgs/kayaking.png";
+import img6 from "../../images/sampleUserImgs/lawngame.png";
+import img7 from "../../images/sampleUserImgs/icecream.png";
+import img8 from "../../images/sampleUserImgs/tenniscourt.png";
+import img9 from "../../images/sampleUserImgs/beach.png";
+
 const Profile = (props) => {
   const authState = useSelector((state) => state.auth);
 
   return (
     <MainContainer>
-      <div>
-        <h1>Profile</h1>
+      <div className={styles.container}>
+        <div className={styles.userIcon}></div>
+        <h2>Tagline</h2>
+        <h1 className={styles.tagline}>{authState.user.tagline}</h1>
+        <h2>Photos</h2>
+        <div className={styles.photoGrid}>
+          <img className={styles.userImg} alt="userImage" src={img1} />
+          <img className={styles.userImg} alt="userImage" src={img2} />
+          <img className={styles.userImg} alt="userImage" src={img3} />
+          <img className={styles.userImg} alt="userImage" src={img4} />
+          <img className={styles.userImg} alt="userImage" src={img5} />
+          <img className={styles.userImg} alt="userImage" src={img6} />
+          <img className={styles.userImg} alt="userImage" src={img7} />
+          <img className={styles.userImg} alt="userImage" src={img8} />
+          <img className={styles.userImg} alt="userImage" src={img9} />
+        </div>
+        <h2>Friends</h2>
+        <div className={styles.friendsContainer}>
+          <div className={styles.friendIcon}></div>
+          <div className={styles.friendIcon}></div>
+          <div className={styles.friendIcon}></div>
+        </div>
       </div>
     </MainContainer>
   );
