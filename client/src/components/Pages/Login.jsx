d244ae9ed78f177c6eb921a7652c1ade67488cf2
macuser <macuser@MacBook-Pro.local>
2023-11-30 16:16:52 -0800

    added login style

--- client/src/components/Pages/Login.jsx
+++ client/src/components/Pages/Login.jsx
@@ -10,8 +10,20 @@ import SimpleButton from "../UI/SimpleButton";
 import { modalActions } from "../../store/modal-slice";
 import { popupActions } from "../../store/popup-slice";
 import LoaderSpinner from "../UI/LoaderSpinner";
+import biking from "../../images/_biking.png";
+import hiking from "../../images/_hiking.png";
+import playingball from "../../images/_playingball.png";
+import music from "../../images/_music.png";
+import tabletennis from "../../images/_tabletennis.png";
+import logo from "../../images/frolik2.png";
 
 const validPreviousPaths = ["/outing", "/profile", "/social", "/chat"];
+
+const flavourImages = [biking, hiking, playingball, music, tabletennis];
+
+const flavourIndex = Math.abs(
+  ((Math.random() * 100) % flavourImages.length).toFixed(0) - 1
+);
 
 function Login() {
   const usernameRef = useRef();
@@ -26,7 +38,7 @@ function Login() {
     if (authState.isAuthenticated) {
       const prevUrl = localStorage.getItem("previousUrl");
       navigate((validPreviousPaths.includes(prevUrl) && prevUrl) || "/profile");
-    } 
+    }
   }, [authState.isAuthenticated, navigate]);
 
   const handleSubmit = (e) => {
@@ -63,75 +75,87 @@ function Login() {
     navigate("/send-reset-password-link");
   };
 
-  const onSampleLogin = (e) =>{
-    usernameRef.current.value = 'app.frolik@gmail.com'
-    passwordRef.current.value = 'frolik1*'
-    handleSubmit(e)
-  }
+  const onSampleLogin = (e) => {
+    usernameRef.current.value = "app.frolik@gmail.com";
+    passwordRef.current.value = "frolik1*";
+    handleSubmit(e);
+  };
 
   return (
     <div className={styles["login-page"]}>
       <CreateAccountModal />
-      <form className={styles["login-form"]} onSubmit={handleSubmit}>
-        <h1 className={styles.header}>Login!</h1>
-        <div>
-          <SimpleInput
-            type="text"
-            id="username"
-            name="username"
-            label="Email:"
-            ref={usernameRef}
-            defaultVal={""}
-            required
-          />
-        </div>
-        <div>
-          <SimpleInput
-            type="password"
-            id="password"
-            name="password"
-            label="Password:"
-            ref={passwordRef}
-            defaultVal={""}
-            required
-          />
-        </div>
+      <div className={styles.formContainer}>
+        <form className={styles["login-form"]} onSubmit={handleSubmit}>
+          <h1 className={styles.header}>Login</h1>
+          <div>
+            <SimpleInput
+              type="text"
+              id="username"
+              name="username"
+              label="Email:"
+              ref={usernameRef}
+              defaultVal={""}
+              required
+            />
+          </div>
+          <div>
+            <SimpleInput
+              type="password"
+              id="password"
+              name="password"
+              label="Password:"
+              ref={passwordRef}
+              defaultVal={""}
+              required
+            />
+          </div>
 
-        <SimpleButton
-          noShadow={true}
-          className={styles.loginButton}
-          type={"submit"}
-        >
-          {!isLoggingIn && !authState.isAuthenticated ? (
-            "Login"
-          ) : (
-            <div className={styles.loaderContainer}>
-              Loggin In &nbsp; <LoaderSpinner width="1.5rem" height="1.5rem" />
-            </div>
-          )}
-        </SimpleButton>
-        <SimpleButton
-          noShadow={true}
-          onClick={handleCreateAccountButtonClick}
-          className={styles.createAccountButton}
-        >
-          Create Profile
-        </SimpleButton>
-        <div className={styles.smallButtons}>
-          <button
-            onClick={onForgotPasswordClick}
-            className={styles.forgotButton}
+          <SimpleButton
+            noShadow={true}
+            className={styles.loginButton}
+            type={"submit"}
           >
-            Forgot Password
-          </button>
-          <button
-            onClick={onSampleLogin}
-            className={styles.forgotButton}
+            {!isLoggingIn && !authState.isAuthenticated ? (
+              "Login"
+            ) : (
+              <div className={styles.loaderContainer}>
+                Loggin In &nbsp;{" "}
+                <LoaderSpinner width="1.5rem" height="1.5rem" />
+              </div>
+            )}
+          </SimpleButton>
+          <SimpleButton
+            noShadow={true}
+            onClick={handleCreateAccountButtonClick}
+            className={styles.createAccountButton}
           >
-            Sample Login
-          </button>
+            Create Profile
+          </SimpleButton>
+          <div className={styles.smallButtons}>
+            <button
+              onClick={onForgotPasswordClick}
+              className={styles.forgotButton}
+            >
+              Forgot Password
+            </button>
+            <button onClick={onSampleLogin} className={styles.forgotButton}>
+              Sample Login
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className={styles.flavourContainer}>
+        <div className={styles.appBlurb}>
+          <img className={styles.logo} src={logo} alt="logo" /> is a social
+          media platform designed to engage people to make new connections and
+          get out of the house!
         </div>
-      </form>
+        <img
+          className={styles.flavourImage}
+          alt="froliking"
+          src={flavourImages[flavourIndex]}
+        />
+      </div>
     </div>
   );
 }
