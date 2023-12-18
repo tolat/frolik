4a152ee8d4f46a889055551c3dc60495c398074e
macuser <macuser@MacBook-Pro.local>
2023-12-18 14:09:39 -0800

    updating styles for mobile

--- client/src/components/UI/MainContainer.jsx
+++ client/src/components/UI/MainContainer.jsx
@@ -1,12 +1,25 @@
+import { useLocation } from "react-router-dom";
 import SlideInModal from "../Modals/SlideInModal";
 import styles from "./styles/MainContainer.module.scss";
+import { useEffect, useState } from "react";
 
 const MainContainer = (props) => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  const [marginBottom, setMarginBottom] = useState("");
+
+  useEffect(() => {
+    if (currentUrl.includes("login")) {
+      setMarginBottom("0");
+    } else {
+      setMarginBottom("");
+    }
+  }, [currentUrl]);
   return (
-    <div className={styles.outer}>
+    <div className={styles.outer} style={{ marginBottom: marginBottom }}>
       <div id="main-container" className={styles.inner}>
-         {props.children}
-          <SlideInModal />
+        {props.children}
+        <SlideInModal />
       </div>
     </div>
   );
