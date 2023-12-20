b7b358de60a06ae686831a25580a850e5318c1ee
macuser <macuser@MacBook-Pro.local>
2023-12-19 21:28:06 -0800

    starting to add a serviceWorker

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const webpush = require("web-push");
 
 router.get("/vapid-public-key", (req, res) => {
   res.send({ key: process.env.VAPID_PUBLIC_KEY });
 });
 
 router.post("/push/subscribe", (req, res) => {
+  const subscription = req.body;
+  console.log(
+    "SUBSCRIPTION:",
+    subscription,
+    "ENDPOINT: ",
+    subscription.endpoint
+  );
 
-  console.log(req.body);
+  const notificationPayload = {
+    title: "Notification Title",
+    body: "Notification Body",
+  };
+
+  webpush
+    .sendNotification(subscription, JSON.stringify(notificationPayload))
+    .then(() => {
+      res.status(200).json({ message: "Notification sent" });
+    })
+    .catch((error) => {
+      console.error("Error sending notification:", error);
+      res.status(500).json({ error: "Failed to send notification" });
+    });
 });
 
 module.exports = router;
