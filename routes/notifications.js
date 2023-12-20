fd38170a7c315bb11af698dbdd9b1d154d828891
macuser <macuser@MacBook-Pro.local>
2023-12-19 21:52:29 -0800

    starting to add a serviceWorker

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,34 +2,32 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const webpush = require("web-push");
 
+webpush.setVapidDetails(
+  `mailto:${process.env.SENDMAIL_FROM}`,
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+);
+
 router.get("/vapid-public-key", (req, res) => {
   res.send({ key: process.env.VAPID_PUBLIC_KEY });
 });
 
 router.post("/push/subscribe", (req, res) => {
   const subscription = req.body;
-  console.log(
-    "SUBSCRIPTION:",
-    subscription,
-    "ENDPOINT: ",
-    subscription.endpoint
-  );
 
-  const notificationPayload = {
-    title: "Notification Title",
-    body: "Notification Body",
-  };
+  const payload = JSON.stringify({
+    title: "Testing",
+    body: "Hope This Works",
+  });
 
   webpush
-    .sendNotification(subscription, JSON.stringify(notificationPayload))
+    .sendNotification(subscription, payload)
     .then(() => {
-      console.log("notification sent");
-      res.status(200).json({ message: "Notification sent" });
+      console.log("NOTIFICATION SENT");
     })
     .catch((error) => {
       console.log("notification not sent: ", error);
       console.error("Error sending notification:", error);
-      res.status(500).json({ error: "Failed to send notification" });
     });
 });
 
